c28d0799a8c3215b068970fbfcc59bad9329821d
PD4272 <30520206+PD4272@users.noreply.github.com>
2019-12-03 17:16:09 +0530

    Delete background.js

--- Developer Extension/js/background.js
+++
@@ -1,9 +0,0 @@
-/* $Id$ */
-
-chrome.browserAction.onClicked.addListener( function (tab) {
-	chrome.tabs.create({'url': chrome.extension.getURL("html/zwplugin.html")}, function(tab) {
-    // Tab opened.
-		
-  })
- 
-});
